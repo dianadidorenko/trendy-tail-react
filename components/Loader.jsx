fbca995a2eb868a727bbf00236a009509166b381
Диана <diana@MacBook-Pro-Diana.local>
2024-07-28 18:55:41 +0300

    fix clerk env

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -1,8 +1,9 @@
 const Loader = () => {
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="animate-spin rounded-full border-t-4 border-darkBlueColor border-solid h-12 w-12"></div>
+      <div className="animate-spin rounded-full border-t-4 border-darkBlueColor border-solid h-13 w-13"></div>
     </div>
+
   );
 };
 
